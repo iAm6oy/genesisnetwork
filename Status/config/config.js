b2e9994194dd39335fc7ee1c4bbe18274fdd47d5
iAm6oy <65881793+iAm6oy@users.noreply.github.com>
2020-11-05 00:38:10 -0600

    Add files via upload

---
+++ Status/config/config.js
@@ -0,0 +1,37 @@
+const variables = {
+	title: "Index || WebServer",
+
+	favicon: "./images/icon.png",
+
+	description: "Minecraft Network Server",
+
+	principalText: "WebServer Minecraft",
+
+	playerCounterText: "%pl-online% players online",
+
+	serverIp: "mc.hypixel.net",
+
+	copiedText: "Ip has been copied",
+
+	
+
+	buttons: {
+		forum: ["FORUM", "#forum"],
+
+		store: ["STORE", "#store"],
+
+		vote: ["VOTE", "#vote"]
+	},
+
+
+
+	style: {
+		backgroundColor: "#000000db",
+
+		backgroundImageName: "./images/background.jpg",
+
+		logoImageName: "./images/logo.png",
+
+		logoAnimationFloat: true
+	}
+}
