e79f22568f2c8492eae3252e2d5d347ccbeeb66d
iAm6oy <65881793+iAm6oy@users.noreply.github.com>
2020-11-09 13:13:37 -0600

    update

---
+++ config/config.js
@@ -0,0 +1,25 @@
+const variables = {
+	title: "Genesis Network | Status",
+
+	favicon: "../images/genesislogo.png",
+
+	description: "Status of Genesis Network",
+
+	principalText: "Genesis Network",
+
+	playerCounterText: "%pl-online% players online",
+
+	serverIp: "genesish.cf",
+
+	copiedText: "IP has been copied",
+
+	style: {
+		backgroundColor: "#000000db",
+
+		backgroundImageName: "../images/genesishcfbg.jpg",
+
+		logoImageName: "../images/genesislogo.png",
+
+		logoAnimationFloat: true
+	}
+}
